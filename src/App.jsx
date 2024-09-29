6fb9bd8700b805f05c7e334355c316113eb26ed8
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-09-30 00:19:59 +0100

    working on the footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
-
+import Footer from "./components/Footer/Footer";
 export default function App() {
   return (
     <>
       <Navigation />
       <Header />
+      <Footer />
     </>
   );
 }
